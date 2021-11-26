01548a6011ab97b8957fa14d7873fe7d01ab70ce
Juan Ontiveros <juan.ontiveros@zerf.com.ar>
2021-11-26 00:43:21 -0300

    Agregando al login redireccion a la home

--- src/app/screens/Login/index.js
+++ src/app/screens/Login/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { string, bool } from 'prop-types';
+import { useHistory } from 'react-router';
 
 import FormWrapper from '~app/components/FormWrapper';
 import Header from '~app/components/Header';
+import { ROUTES } from '~constants/routes';
 
 import { INITIAL_VALUES } from './constants';
 import styles from './styles.module.scss';
@@ -11,8 +13,10 @@ import Links from './components/Links';
 import { VALIDATION_SCHEMA } from './utils';
 
 function Login({ title, signUp }) {
+  const history = useHistory();
   const handleSubmit = () => {
     // TODO: handle login
+    history.push(ROUTES.HOME.path);
   };
   return (
     <div className={`row full-width full-height center middle space-between ${styles.container}`}>
